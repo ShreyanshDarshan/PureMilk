4f64ffe02ecac828d22ca22f60d6e6a674cef7e9
Shreyansh Darshan <shreyanshdarshan1@gmail.com>
2023-01-20 21:11:28 +0530

    Add dataset and model class

---
+++ PureMilk/dataset.py
@@ -0,0 +1,31 @@
+import os
+import torch
+from torch.utils.data import Dataset
+from skimage import io
+import pandas as pd
+
+
+class AdulterantDataset(Dataset):
+
+    def __init__(self, dataset_path, tarnsform=None):
+        self.root_dir = dataset_path
+
+        csv_file_path = os.path.join(self.root_dir, "concentration_data.csv")
+        if not os.path.exists(csv_file_path):
+            raise ValueError("CSV file not found")
+        self.concentrations = pd.read_csv(csv_file_path)
+
+        self.transform = tarnsform
+
+    def __len__(self):
+        return len(self.concentrations)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.concentrations.iloc[index, 0])
+        image = io.imread(img_path)
+        y_label = torch.tensor(self.concentrations.iloc[index, 1])
+
+        if self.transform:
+            image = self.transform(image)
+
+        return (image, y_label)
---
+++ PureMilk/model.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+class AdulterantDetector(nn.Module):
+
+    def __init__(self, layers, encoding_dim=10):
+        super(AdulterantDetector, self).__init__()
+
+        if layers[0] % 2 * encoding_dim != 0:
+            raise ValueError("Input size must be a multiple of 2*encoding_dim")
+        self.encoding_dim = encoding_dim
+
+        self.layers = []
+        for i in range(len(layers) - 2):
+            self.layers.append(nn.Linear(layers[i], layers[i + 1]))
+            self.layers.append(nn.ReLU())
+        self.layers.append(nn.Linear(layers[-2], layers[-1]))
+        self.sequential = nn.Sequential(*self.layers)
+
+    def forward(self, x):
+        out = self.sequential(x)
+        return
